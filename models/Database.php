79325c91d3e2df5e0924ae31bee1a26e87a08edc
copgADSI <48689287+copgADSI@users.noreply.github.com>
2023-03-09 16:50:54 -0500

    Se inicializa la clase del database

---
+++ models/Database.php
@@ -0,0 +1,12 @@
+<?php 
+class Database {
+    private $localhost = '';
+    private $user = 'root';
+    private $password = '';
+    private $db_name = 'proyect_adso';
+
+    public static function getConnection()
+    {
+        # code...
+    }
+}
